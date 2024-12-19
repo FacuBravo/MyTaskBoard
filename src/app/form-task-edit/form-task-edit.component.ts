68d427b12df6e04e7ef2e4e00b783fda52374f10
Facundo Bravo <bfacundo176@gmail.com>
2024-12-18 23:11:19 -0300

    Terminada estructura formulario

--- src/app/form-task-edit/form-task-edit.component.ts
+++ src/app/form-task-edit/form-task-edit.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './form-task-edit.component.css'
 })
 export class FormTaskEditComponent {
+  selectIcon(e: Event) {
+    e.preventDefault()
 
+    document.querySelectorAll('.icon_btn').forEach(el => {
+      el.setAttribute('style', 'background-color: var(--grey_200);')
+    })
+
+    const clickedBtn = e.target as HTMLButtonElement
+    clickedBtn.style.backgroundColor = 'var(--yellow_500)'
+  }
+
+  selectStatus(e: Event) {
+    e.preventDefault()
+
+    document.querySelectorAll('.status_button').forEach(el => {
+      el.classList.remove('status_selected')
+    })
+
+    const clickedBtn = e.target as HTMLButtonElement
+    clickedBtn.classList.add('status_selected')
+  }
 }

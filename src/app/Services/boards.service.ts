4e344bfb0ef571eb719cfd57198ab757ad4d3062
Facundo Bravo <bfacundo176@gmail.com>
2024-12-19 14:28:25 -0300

    Mejorado boardService

--- src/app/Services/boards.service.ts
+++ src/app/Services/boards.service.ts
@@ -8,9 +8,10 @@ const URL = 'https://6764340b52b2a7619f5bcf4a.mockapi.io/boards'
 })
 export class BoardsService {
 
-  async getBoards(): Promise<Board[]> {
-    const res = await fetch(URL)
-    return await res.json()
+  async getBoardByIp(ip: string): Promise<Board> {
+    const res = await fetch(URL + '?ip=' + ip)
+    const json = await res.json()
+    return json[0]
   }
 
   async getBoard(id: number): Promise<Board> {

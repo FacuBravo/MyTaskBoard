a6f39abf989fbd45c6753eaf87e57b4f7e768d6e
Facundo Bravo <bfacundo176@gmail.com>
2024-12-19 00:24:22 -0300

    Import sin uso

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,12 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../types/Task';
 import { FormTaskEditComponent } from "../form-task-edit/form-task-edit.component";
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-edit',
   standalone: true,
-  imports: [FormTaskEditComponent, JsonPipe],
+  imports: [FormTaskEditComponent],
   templateUrl: './task-edit.component.html',
   styleUrl: './task-edit.component.css'
 })
